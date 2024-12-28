8fd3dbef329da41e9f9124b897726d710dbd1bb3
JoeG67 <joachimgerard66@gmail.com>
2024-12-28 15:28:40 +0800

    Add in all planned navbar icons entirely (search bar, logo, etc), pending rearrangement

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <header
-  class="fixed top-0 left-0 w-full flex flex-col items-center lg:flex-row lg:items-center lg:pr-8 lg:space-x-8 bg-white shadow-md z-10"  >
+    class="fixed top-0 left-0 w-full flex flex-col items-center lg:flex-row lg:items-center lg:pr-8 lg:space-x-8 bg-white shadow-md z-10"
+  >
     <NaviBar />
   </header>
 
-  <main>
-    <div class="flex flex-wrap items-start"></div>
+  <main class="mt-16">
+    <div class="flex flex-wrap items-start">
+    </div>
   </main>
 </template>
+
 
 <script setup lang="ts">
 import PrimeVue from 'primevue/config';
