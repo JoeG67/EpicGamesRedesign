30c546bc37ea59c7e3a48ec258002b7519c41918
JoeG67 <joachimgerard66@gmail.com>
2024-12-29 17:24:06 +0800

    Updated existing tailwind classes

--- src/components/NaviBar.vue
+++ src/components/NaviBar.vue
@@ -13,27 +13,25 @@
         </div>
       </template>
       <template #center>
-  <InputText
-    placeholder="Search"
-    class="bg-transparent outline-none w-full placeholder-gray-500 text-gray-700 "
-    />
-  <i class="pi pi-search"></i>
+        <div class="text-black font-normal border-2 border-gray-400 rounded-lg py-2 px-2">
+          <InputText placeholder="Search" />
+          <i class="pi pi-search"></i>
+        </div>
+      </template>
+      <template #end>
+        <div class="flex mr-6">
+          <Button
+            :icon="'pi pi-users'"
+            class="p-button p-component p-button-text p-button-plain mr-5"
+          />
+          <Button :icon="'pi pi-heart'" class="text-gray-600 mr-5" />
 
-            </template>
-            <template #end>
-              <div class="flex mr-6">
-    <Button :icon="'pi pi-users'" text plain class="text-gray-600 mr-5" />
-    <Button :icon="'pi pi-heart'" severity="secondary" text class="text-gray-600" />
-
-    <Avatar
-      image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-      style="width: 32px; height: 32px"
-    />
-</div>
-
-
-</template>
-
+          <Avatar
+            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+            style="width: 32px; height: 32px"
+          />
+        </div>
+      </template>
     </Toolbar>
   </div>
 </template>
@@ -42,3 +40,9 @@
 import Toolbar from 'primevue/toolbar'
 import { Button, IconField, InputText, InputIcon, Avatar } from 'primevue'
 </script>
+
+<style>
+::deep(.p-inputtext) {
+  color: black !important; /* Forces text color */
+}
+</style>
