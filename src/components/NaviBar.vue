ecc95a6b64cc6bcba44bea5a2efd8175e04acf73
JoeG67 <joachimgerard66@gmail.com>
2025-01-04 20:52:09 +0800

    Add in all planned navbar icons entirely (search bar, logo, etc)

--- src/components/NaviBar.vue
+++ src/components/NaviBar.vue
@@ -14,22 +14,43 @@
       </template>
       <template #center>
         <div class="text-black font-normal border-2 border-gray-400 rounded-lg py-2 px-2">
-          <InputText placeholder="Search" />
+          <InputText placeholder="Search store" />
           <i class="pi pi-search"></i>
         </div>
       </template>
       <template #end>
-        <div class="flex mr-6">
-          <Button
-            :icon="'pi pi-users'"
-            class="p-button p-component p-button-text p-button-plain mr-5"
-          />
+        <div class="flex">
+          <Button :icon="'pi pi-list'" class="text-gray-600 mr-5" />
+          <Button :icon="'pi pi-users'" class="text-gray-600 mr-5" />
+
           <Button :icon="'pi pi-shopping-cart'" class="text-gray-600 mr-5" />
 
-          <Avatar
-            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-            style="width: 32px; height: 32px"
-          />
+          <Button :icon="'pi pi-user'" class="text-gray-600 mr-5" @click="visible = true" />
+          <Drawer v-model:visible="visible" position="right" class="!bg-[#2F2D2E]">
+            <template #header>
+              <div class="flex items-center gap-2">
+                <Avatar image="/images/avatar/amyelsner.png" shape="circle" />
+                <span class="font-bold">Amy Elsner</span>
+              </div>
+            </template>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <template #footer>
+              <div class="flex items-center gap-2">
+                <Button label="Account" icon="pi pi-user" class="flex-auto" outlined />
+                <Button
+                  label="Logout"
+                  icon="pi pi-sign-out"
+                  class="flex-auto"
+                  severity="danger"
+                  text
+                />
+              </div>
+            </template>
+          </Drawer>
         </div>
       </template>
     </Toolbar>
@@ -38,7 +59,10 @@
 
 <script setup lang="ts">
 import Toolbar from 'primevue/toolbar'
-import { Button, IconField, InputText, InputIcon, Avatar } from 'primevue'
+import { Button, InputText, Drawer, Avatar } from 'primevue'
+import { ref } from 'vue'
+
+const visible = ref(false)
 </script>
 
 <style>
