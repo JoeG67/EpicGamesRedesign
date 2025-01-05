b56e48ea37b8b8d59951770327d36e71a4c24eb2
JoeG67 <joachimgerard66@gmail.com>
2025-01-05 15:08:17 +0800

    Added in new homepage component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
   </header>
 
   <main class="mt-16">
-    <div class="flex flex-wrap items-start">
+    <div class="flex">
+      <HomePage />
     </div>
   </main>
 </template>
@@ -16,7 +17,9 @@ import PrimeVue from 'primevue/config';
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import NaviBar from './components/NaviBar.vue'
+import HomePage from './components/HomePage.vue'
+
 import '../dist/output.css';
-import './assets/main.css'; 
+import './assets/main.css';
 
 </script>
